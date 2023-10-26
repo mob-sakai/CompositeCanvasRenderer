04c0f3f25ed72a68e42883509ed0811306c1839d
mob-sakai <sakai861104@gmail.com>
2023-10-26 15:40:36 +0900

    feat: add shadow and outline effects

---
+++ Packages/src/Runtime/Effects/CompositeCanvasOutline.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CompositeCanvas
+{
+    public class CompositeCanvasOutline : CompositeCanvasShadow
+    {
+        public enum OutlinePattern
+        {
+            x0,
+            x1,
+            x4,
+            x8
+        }
+
+        [SerializeField]
+        private OutlinePattern m_OutlinePattern = OutlinePattern.x4;
+
+        public OutlinePattern outlinePattern
+        {
+            get => m_OutlinePattern;
+            set
+            {
+                if (m_OutlinePattern == value) return;
+
+                m_OutlinePattern = value;
+                SetRendererVerticesDirty();
+            }
+        }
+
+        public override void ModifyMesh(VertexHelper vh)
+        {
+            if (!isActiveAndEnabled) return;
+
+            if (OutlinePattern.x0 == outlinePattern)
+            {
+                vh.Clear();
+                return;
+            }
+
+            var count = vh.currentVertCount;
+            var x = effectDistance.x;
+            var y = effectDistance.y;
+            Color32 c = compositeCanvasRenderer.color;
+
+            if (OutlinePattern.x8 <= outlinePattern)
+            {
+                ApplyShadowZeroAlloc(true, vh, count, +x, 0, c);
+                ApplyShadowZeroAlloc(true, vh, count, -x, 0, c);
+                ApplyShadowZeroAlloc(true, vh, count, 0, +y, c);
+                ApplyShadowZeroAlloc(true, vh, count, 0, -y, c);
+            }
+
+            if (OutlinePattern.x4 <= outlinePattern)
+            {
+                ApplyShadowZeroAlloc(true, vh, count, +x, -y, c);
+                ApplyShadowZeroAlloc(true, vh, count, -x, +y, c);
+                ApplyShadowZeroAlloc(true, vh, count, -x, -y, c);
+            }
+
+            ApplyShadowZeroAlloc(false, vh, count, +x, +y, c);
+        }
+    }
+}
---
+++ Packages/src/Runtime/Effects/CompositeCanvasShadow.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.UI;
+
+namespace CompositeCanvas
+{
+    public class CompositeCanvasShadow : CompositeCanvasBlur, IMeshModifier
+    {
+        [Header("Effect Settings")]
+        [SerializeField]
+        private Vector2 m_EffectDistance = new Vector2(1, -1);
+
+        public Vector2 effectDistance
+        {
+            get => m_EffectDistance;
+            set
+            {
+                if (m_EffectDistance == value)
+                {
+                    return;
+                }
+
+                m_EffectDistance = value;
+                SetRendererVerticesDirty();
+            }
+        }
+
+        public override void Reset()
+        {
+            if (!compositeCanvasRenderer) return;
+            compositeCanvasRenderer.colorMode = ColorMode.Fill;
+            compositeCanvasRenderer.blendType = BlendType.AlphaBlend;
+            compositeCanvasRenderer.foreground = false;
+            compositeCanvasRenderer.showSourceGraphics = true;
+            compositeCanvasRenderer.color = new Color(0, 1, 0, 1f);
+        }
+
+        protected override void OnEnable()
+        {
+            SetRendererDirty();
+            SetRendererVerticesDirty();
+        }
+
+        protected override void OnDisable()
+        {
+            SetRendererDirty();
+            SetRendererVerticesDirty();
+        }
+
+        protected override void OnValidate()
+        {
+            SetRendererDirty();
+            SetRendererVerticesDirty();
+        }
+
+        public void ModifyMesh(Mesh mesh)
+        {
+        }
+
+        public virtual void ModifyMesh(VertexHelper vh)
+        {
+            if (!isActiveAndEnabled || !compositeCanvasRenderer) return;
+
+            Profiler.BeginSample("(CCR)[CompositeCanvasShadow] ModifyMesh > Add shadow");
+            var count = vh.currentVertCount;
+            var x = effectDistance.x;
+            var y = effectDistance.y;
+            Color32 c = compositeCanvasRenderer.color;
+            ApplyShadowZeroAlloc(false, vh, count, x, y, c);
+            Profiler.EndSample();
+        }
+
+        protected static void ApplyShadowZeroAlloc(bool add, VertexHelper vh, int count, float x, float y, Color32 c)
+        {
+            if (add)
+            {
+                Profiler.BeginSample("(CCR)[CompositeCanvasShadow] ModifyMesh > Add Triangle");
+                var start = vh.currentVertCount;
+                vh.AddTriangle(start + 0, start + 1, start + 2);
+                vh.AddTriangle(start + 2, start + 3, start + 0);
+                Profiler.EndSample();
+            }
+
+            var vt = new UIVertex();
+            for (var i = 0; i < count; i++)
+            {
+                vh.PopulateUIVertex(ref vt, i);
+                var position = vt.position;
+                position.x += x;
+                position.y += y;
+                vt.position = position;
+                vt.color = c;
+
+                if (add)
+                {
+                    Profiler.BeginSample("(CCR)[CompositeCanvasShadow] ModifyMesh > Add Vert");
+                    vh.AddVert(vt);
+                    Profiler.EndSample();
+                }
+                else
+                {
+                    vh.SetUIVertex(vt, i);
+                }
+            }
+        }
+    }
+}
